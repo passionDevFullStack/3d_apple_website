29da4364b7a32f55a56497eebef6a7602cbbd831
Passionate <jrobert050515@gmail.com>
2025-07-17 13:07:13 +0530

    features, how it works and footer section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        blue: "#2997FF",
+        blue: {
+          DEFAULT: "#2997FF",
+          500: "#2997FF",
+        },
         gray: {
+          ...colors.gray,
           DEFAULT: "#86868b",
           100: "#94928d",
           200: "#afafaf",
           300: "#42424570",
         },
-        zinc: "#101010",
+        zinc: {
+          DEFAULT: "#101010",
+        },
       },
     },
   },
