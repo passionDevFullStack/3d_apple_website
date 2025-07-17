e6cb8690b49f21459dda37ab33d149cd10d1f82f
Passionate <jrobert050515@gmail.com>
2025-07-17 13:08:01 +0530

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,40 @@ function Navbar() {
   return (
     <header className="w-full py-5 sm:px-10 px-5  flex justify-between items-center">
       <nav className="flex w-full  screen-max-width  ">
-        <img src={appleImg} alt="apple" width={14} height={18} />
+        <img
+          src={appleImg}
+          alt="apple"
+          width={16}
+          height={22}
+          className="cursor-pointer"
+        />
         <div className="flex flex-1 justify-center max-sm:hidden ">
           {navLists.map((nav, i) => {
             return (
               <div
-                className="px-5 text-sm cursor-pointer text-[#808080] hover:text-white transition-all "
+                className="px-5 text-[15px] cursor-pointer text-[#808080] hover:text-white hover:scale-105 transition-all duration-200 "
                 key={nav}
               >
-                {nav}{" "}
+                {nav}
               </div>
             );
           })}
         </div>
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src={searchImg} alt="search" width={14} height={18} />
-          <img src={bagImg} alt="bag" width={14} height={18} />
+          <img
+            src={searchImg}
+            alt="search"
+            width={16}
+            height={18}
+            className="cursor-pointer hover:scale-110 transition-all duration-200"
+          />
+          <img
+            src={bagImg}
+            alt="bag"
+            width={16}
+            height={18}
+            className="cursor-pointer hover:scale-110  transition-all duration-200"
+          />
         </div>
       </nav>
     </header>
